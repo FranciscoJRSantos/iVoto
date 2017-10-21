1f26dcd4f17977be4b27bf4c48b707cfe4a8974c
kYwzor <tiago_gomes_76@hotmail.com>
2017-10-21 23:24:05 +0100

    simple connections

--- TCP/TCPClient.java
+++ TCP/TCPClient.java
@@ -18,42 +18,45 @@ import java.util.*;
  * @version 1.1
  */
 class TCPClient {
-  public static void main(String[] args) {
-    Socket socket;
-    PrintWriter outToServer;
-    BufferedReader inFromServer = null;
-    try {
-      // connect to the specified address:port (default is localhost:12345)
-      if(args.length == 2)
-        socket = new Socket(args[0], Integer.parseInt(args[1]));
-      else
-        socket = new Socket("localhost", 12345);
-        
-      // create streams for writing to and reading from the socket
-      inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-      outToServer = new PrintWriter(socket.getOutputStream(), true);
+    public static void main(String[] args) {
+        Socket socket;
+        PrintWriter outToServer;
+        BufferedReader inFromServer = null;
+        try {
+            // connect to the specified address:port (default is localhost:12345)
+            if (args.length == 2)
+                socket = new Socket(args[0], Integer.parseInt(args[1]));
+            else
+                socket = new Socket("localhost", 12345);
 
-      // create a thread for reading from the keyboard and writing to the server
-      new Thread() {
-        public void run() {
-          Scanner keyboardScanner = new Scanner(System.in);
-          while(!socket.isClosed()) {
-            String readKeyboard = keyboardScanner.nextLine();
-            outToServer.println(readKeyboard);
-          }
+            // create streams for writing to and reading from the socket
+            inFromServer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            outToServer = new PrintWriter(socket.getOutputStream(), true);
+
+            // create a thread for reading from the keyboard and writing to the server
+            new Thread() {
+                public void run() {
+                    Scanner keyboardScanner = new Scanner(System.in);
+                    while (!socket.isClosed()) {
+                        String readKeyboard = keyboardScanner.nextLine();
+                        outToServer.println(readKeyboard);
+                    }
+                }
+            }.start();
+
+            // the main thread loops reading from the server and writing to System.out
+            String messageFromServer;
+            while ((messageFromServer = inFromServer.readLine()) != null)
+                System.out.println(messageFromServer);
+        } catch (IOException e) {
+            if (inFromServer == null)
+                System.out.println("\nUsage: java TCPClient <host> <port>\n");
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                inFromServer.close();
+            } catch (Exception e) {
+            }
         }
-      }.start();
-
-      // the main thread loops reading from the server and writing to System.out
-      String messageFromServer;
-      while((messageFromServer = inFromServer.readLine()) != null)
-        System.out.println(messageFromServer);
-    } catch (IOException e) {
-      if(inFromServer == null)
-        System.out.println("\nUsage: java TCPClient <host> <port>\n");
-      System.out.println(e.getMessage());
-    } finally {
-      try { inFromServer.close(); } catch (Exception e) {}
     }
-  }
 }
---
+++ TCP/TCPServer.java
@@ -0,0 +1,59 @@
+// TCPServer2.java: Multithreaded server
+
+import java.net.*;
+import java.io.*;
+
+public class TCPServer {
+    public static void main(String args[]) {
+        int numero = 0;
+
+        try {
+            int serverPort = 12345;
+            System.out.println("Listening on port :" + serverPort);
+            ServerSocket listenSocket = new ServerSocket(serverPort);
+            System.out.println("LISTEN SOCKET=" + listenSocket);
+            while (true) {
+                Socket clientSocket = listenSocket.accept();
+                System.out.println("CLIENT_SOCKET (created at accept())=" + clientSocket);
+                numero++;
+                new Connection(clientSocket, numero);
+            }
+        } catch (IOException e) {
+            System.out.println("Listen:" + e.getMessage());
+        }
+    }
+}
+
+//= Thread para tratar de cada canal de comunicação com um cliente
+class Connection extends Thread {
+    BufferedReader in;
+    PrintWriter out;
+    Socket clientSocket;
+    int thread_number;
+
+    public Connection(Socket aClientSocket, int numero) {
+        thread_number = numero;
+        try {
+            clientSocket = aClientSocket;
+            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            this.start();
+        } catch (IOException e) {
+            System.out.println("Connection:" + e.getMessage());
+        }
+    }
+
+    //=============================
+    public void run() {
+        String clientMessage;
+        try {
+            while ((clientMessage = in.readLine()) != null) {
+                System.out.println("T["+thread_number + "] Received: "+ clientMessage);
+                out.println("Server received: " + clientMessage);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
