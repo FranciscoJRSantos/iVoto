d115d25dd3a9e81ddeb723a2246614e6b7784d35
kYwzor <tiago_gomes_76@hotmail.com>
2017-10-24 21:46:30 +0100

    Added properties stuff

---
+++ TCP/TCPConfigLoader.java
@@ -0,0 +1,41 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class TCPConfigLoader{
+
+    private int tcpPort;
+
+    public TCPConfigLoader(){
+        Properties prop = new java.util.Properties();
+        FileInputStream input = null;
+
+        try{
+            input = new FileInputStream("tcp.properties");
+
+            if(input == null){
+                System.out.println("File could not be located!");
+                return;
+            }
+
+            prop.load(input);
+
+            this.tcpPort = Integer.parseInt(prop.getProperty("tcpPort"));
+
+
+        } catch (IOException ioe){
+            ioe.printStackTrace();
+        } finally {
+            if (input != null){
+                try {
+                    input.close();
+                } catch (IOException ioe){
+                    ioe.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public int getTCPPort() { return this.tcpPort; }
+
+}
